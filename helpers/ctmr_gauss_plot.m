79970e70ba2b91e8b3dbcee3ecbdc2436ee56049
NIKHILESH NATRAJ <nikhilesh.natraj@ucsf.edu>
2025-03-29 08:54:39 +0530

    imaging

---
+++ helpers/ctmr_gauss_plot.m
@@ -0,0 +1,132 @@
+function [c_h,c] = ctmr_gauss_plot(cortex,electrodes,weights,hemi,do_lighting,do_plot,do_cmap)
+% function [c_h,c] = ctmr_gauss_plot(cortex,electrodes,weights,hemi,do_lighting,do_plot,do_cmap)
+% c_h - figure handle
+% c - interpolated data 
+% projects electrode locations onto their cortical spots in the
+% left hemisphere and plots about them using a gaussian kernel
+% for only cortex use:
+% ctmr_gauss_plot(cortex,[0 0 0],0)
+% rel_dir=which('loc_plot');
+% rel_dir((length(rel_dir)-10):length(rel_dir))=[];
+% addpath(rel_dir)
+% IMPORTANT INPUTS
+% do_cmap: 1-> grey brain, 0 -> silver brain 
+
+%     Copyright (C) 2009  K.J. Miller & D. Hermes, Dept of Neurology and Neurosurgery, University Medical Center Utrecht
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%   Version 1.1.0, released 26-11-2009
+%
+% Edited 2016 by Liberty Hamilton
+% Edited 2021 by Nikhilesh Natraj
+
+if nargin<2
+    electrodes = [0 0 0];
+end
+if nargin<3
+    weights = 0;
+end
+if nargin<4
+    hemi = 'lh';
+end
+if nargin<5
+    do_lighting=1;
+end
+if nargin<6
+    do_plot=1;
+end
+if nargin<7
+    do_cmap=0;
+end
+
+%load in colormap
+% load('loc_colormap')
+load('loc_colormap_thresh')
+if do_cmap
+    cm(31:35,1:3)=.65;
+end
+% load('BlWhRdYl_colormap')
+% load('BlGyOrCp_colormap')
+% cm1 = (cbrewer('div','RdBu',64)); 
+%cm=parula(64);
+ %cm(1:50,:)=cm1(1:50,:);
+% %  
+%  cmm = hot(59*3);
+%  cm(1:30,:) = cmm(1:3:90,:);
+%  cm(36:end,:) = cmm(91:3:end,:);
+ 
+
+brain=cortex.vert;
+v='l';
+
+if length(weights)~=length(electrodes(:,1))
+    error('you sent a different number of weights than electrodes (perhaps a whole matrix instead of vector)')
+end
+
+%gaussian "cortical" spreading parameter - in mm, so if set at 10, its 1 cm
+%- distance between adjacent electrodes
+gsp=10; 
+
+c=zeros(length(cortex(:,1)),1);
+for i=1:length(electrodes(:,1))
+    b_z=abs(brain(:,3)-electrodes(i,3));
+    b_y=abs(brain(:,2)-electrodes(i,2));
+    b_x=abs(brain(:,1)-electrodes(i,1));
+    %%%% parameters for interpolation
+    %d=weights(i)*exp((-(b_x.^2+b_z.^2+b_y.^2).^.5)/gsp^.5); %exponential fall off
+    d=weights(i)*exp((-(b_x.^2+b_z.^2+b_y.^2))/gsp); %gaussian
+    %%%%
+    c=c+d';
+end
+
+% c=(c/max(c));
+if do_plot
+    c_h=tripatch(cortex, 'nofigure', c');
+    shading interp;
+    a=get(gca);
+    
+    %%NOTE: MAY WANT TO MAKE AXIS THE SAME MAGNITUDE ACROSS ALL COMPONENTS TO REFLECT
+    %%RELEVANCE OF CHANNEL FOR COMPARISON's ACROSS CORTICES
+    d=a.CLim;
+    set(gca,'CLim',[-max(abs(d)) max(abs(d))])
+    colormap(cm)
+    %colormap(parula)
+    lighting phong; %play with lighting...
+    %material shiny;
+    material dull;
+    % material([.3 .8 .1 10 1]);
+    % material([.2 .9 .2 50 1]); %  BF: editing mesh viewing attributes
+    axis off
+    
+    if do_lighting
+        l=light;
+        
+        if strcmp(hemi,'lh')
+            view(270, 0);
+            set(l,'Position',[-1 0 0],'Color',[0.8 0.8 0.8]);
+        elseif strcmp(hemi,'rh')
+            view(90, 0);
+            set(l,'Position',[1 0 0],'Color',[0.8 0.8 0.8]);
+        elseif strcmp(hemi, 'top')
+            view(0, 90);
+            set(l,'Position',[0 0 1],'Color',[0.8 0.8 0.8]);
+        elseif strcmp(hemi, 'bottom')
+            view(0, -90);
+            set(l,'Position',[0 0 -1],'Color',[0.8 0.8 0.8]);
+        end
+    end
+else
+    c_h=[];
+end
---
+++ helpers/imaging_B3_waves.m
@@ -0,0 +1,77 @@
+
+
+
+dirn=pwd;
+addpath(genpath('C:\Users\nikic\Documents\MATLAB\ctmr_gauss_plot_April2016\ctmr_gauss_plot_April2016'))
+cd('C:\Users\nikic\Documents\MATLAB\ctmr_gauss_plot_April2016\ctmr_gauss_plot_April2016\Bravo3')
+
+load('BRAVO3_lh_pial')
+%load('BRAVO2_elecs_all2')
+load('grid.mat')
+
+
+ch=1:size(anatomy,1);
+figure
+c_h = ctmr_gauss_plot(cortex,[0 0 0],0,'lh');
+% To plot electrodes with numbers, use the following, as example:
+e_h = el_add(elecmatrix([1:length(ch)],:), 'color', 'b', 'numbers', ch);
+% Or you can just plot them without labels with the default color
+%e_h = el_add(elecmatrix([1:length(ch)],:), 'color', 'r', 'msize',12);
+%e_h = el_add(elecmatrix(1:64,:)); % only loading 48 electrode data
+set(gcf,'Color','w')
+cd(dirn)
+
+
+% plotting by ROI
+figure
+c_h = ctmr_gauss_plot(cortex,[0 0 0],0,'lh',1,1,1);
+set(gcf,'Color','w')
+rois = unique(anatomy(:,4));
+colmap = parula(length(rois));
+for j=1:size(anatomy,1)
+    ch_roi = anatomy{j,4};
+    for i=1:length(rois)
+        if strcmp(ch_roi,rois{i})
+            col = colmap(i,:);
+            e_h = el_add(elecmatrix(j,:), ...
+                'color', col, 'msize',8);
+        end
+    end 
+end
+
+
+% plotting by ROI after correction
+load('anatomy_B3')
+figure
+c_h = ctmr_gauss_plot(cortex,[0 0 0],0,'lh',1,1,1);
+set(gcf,'Color','w')
+rois = unique(anatomy_B3(:,4));
+colmap = parula(length(rois));
+for j=1:size(anatomy_B3,1)
+    ch_roi = anatomy_B3{j,4};
+    for i=1:length(rois)
+        if strcmp(ch_roi,rois{i})
+            col = colmap(i,:);
+            e_h = el_add(elecmatrix(j,:), ...
+                'color', col, 'msize',8);
+        end
+    end 
+end
+
+
+
+% plotting with a color bar denoting the phase values and the radius
+% denoting the amplitude values.
+ph=[linspace(pi,-pi,253)];
+phMap = linspace(-pi,pi,253)';
+ChColorMap=parula(253);
+val = rand(253,1);
+ch=1:253;
+figure;
+c_h = ctmr_gauss_plot(cortex,[0 0 0],0,'lh');
+for j=1:length(val)
+    ms = val(j)*(10-1)+1;
+    [aa bb]=min(abs(ph(j) - phMap));
+    c=ChColorMap(bb,:);
+    e_h = el_add(elecmatrix(j,:), 'color', c,'msize',ms);
+end
