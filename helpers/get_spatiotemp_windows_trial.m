92f34458c252a045c0d9204f47445134007ed08c
NN <nikhilesh.natraj@ucsf.edu>
2025-04-14 23:57:57 -0700

    udpates

--- helpers/get_spatiotemp_windows_trial.m
+++ helpers/get_spatiotemp_windows_trial.m
@@ -36,48 +36,98 @@ for ii=1:length(files)
         l1 = length(data1);
 
         data = [data1;data2;data3;data4];
-        l22=floor(l2/5); % length of the down sampled signal
-        l11=floor(l1/5); % length of the down sampled signal
 
-        % resample and filter
-        data = resample(data,200,1000);
+        % filter at 1Khz
         df = filtfilt(d2,data);
-        df = df(l11+1:end,:);
+
+        % extract only state 2 onwards
+        df =df(l1+1:end,:);
+
+        % downsample to 100Hz
+        df = resample(df,100,1000);
+        %
+        %
+        %
+        %
+        %
+        %         l22=floor(l2/5); % length of the down sampled signal
+        %         l11=floor(l1/5); % length of the down sampled signal
+        %
+        %         % resample and filter
+        %         data = resample(data,200,1000);
+        %         df = filtfilt(d2,data);
+        %         df = df(l11+1:end,:);
+
+        %         % filter at 1khz
+        %         %df = filtfilt(d1,data);
+
 
         % keep track of trial segments
         trial_seg=0;
 
+        % getting 200ms segments
+        %         for xx=1:39:length(df) %39 is the original step forward
+        %             if size(df,1)-xx > 42
+        %                 st = xx;
+        %                 stp = xx+39;
+        %                 yst=xx+1;
+        %                 ystp = xx+1+39;
+        %                 tmpx = df(st:stp,:);
+        %                 tmpy = df(yst:ystp,:);
+        %                 a=[];
+        %                 for j=1:size(tmpx,1)
+        %                     aa = tmpx(j,:);
+        %                     aa = aa(ecog_grid);
+        %                     a(j,:,:) = aa;
+        %                 end
+        %                 b=[];
+        %                 for j=1:size(tmpy,1)
+        %                     bb = tmpy(j,:);
+        %                     bb = bb(ecog_grid);
+        %                     b(j,:,:) = bb;
+        %                 end
+        %
+        %
+        %                 xdata = cat(1,(xdata),(a));
+        %                 ydata = cat(1,(ydata),(b));
+        %                 idx=idx+1;
+        %                 trial_seg = trial_seg+1;
+        %             end
+        %         end
 
-        % filter at 1khz
-        %df = filtfilt(d1,data);
-        for xx=1:39:length(df) %39 is the original step forward
-            if size(df,1)-xx > 42
+        % getting 1s segments with 200ms overlap
+        for xx=1:25:length(df) %39 is the original step forward
+            if size(df,1)-xx >= 100
                 st = xx;
-                stp = xx+39;
-                yst=xx+1;
-                ystp = xx+1+39;
+                stp = xx+99;
+                %yst=xx+1;
+                %ystp = xx+1+39;
                 tmpx = df(st:stp,:);
-                tmpy = df(yst:ystp,:);
+                %tmpy = df(yst:ystp,:);
                 a=[];
                 for j=1:size(tmpx,1)
                     aa = tmpx(j,:);
                     aa = aa(ecog_grid);
                     a(j,:,:) = aa;
                 end
-                b=[];
-                for j=1:size(tmpy,1)
-                    bb = tmpy(j,:);
-                    bb = bb(ecog_grid);
-                    b(j,:,:) = bb;
-                end
+                %                  b=[];
+                %                  for j=1:size(tmpy,1)
+                %                      bb = tmpy(j,:);
+                %                      bb = bb(ecog_grid);
+                %                      b(j,:,:) = bb;
+                %                  end
 
 
                 xdata = cat(1,(xdata),(a));
-                ydata = cat(1,(ydata),(b));
+                %ydata = cat(1,(ydata),(b));
                 idx=idx+1;
                 trial_seg = trial_seg+1;
             end
         end
+
+
+
+
     end
 
     if length(data_trial)==0
@@ -88,9 +138,9 @@ for ii=1:length(files)
 
 
     data_trial(len).xdata=xdata;
-    data_trial(len).ydata=ydata;
+    %data_trial(len).ydata=ydata;
     data_trial(len).TargetID = TrialData.TargetID;
-    
+
     if bci_type==0
         data_trial(len).BCI_context = 'OL';
     elseif    bci_type==1
