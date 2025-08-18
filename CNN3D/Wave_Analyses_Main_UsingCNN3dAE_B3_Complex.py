08db6b7d6df79d51ae30d9448a96ca065fa8f231
NN <nikhilesh.natraj@ucsf.edu>
2025-08-18 03:34:43 -0700

    mods

--- CNN3D/Wave_Analyses_Main_UsingCNN3dAE_B3_Complex.py
+++ CNN3D/Wave_Analyses_Main_UsingCNN3dAE_B3_Complex.py
@@ -182,13 +182,13 @@ for iterr in np.arange(iterations):
     # getparams and train the model 
     num_epochs=100
     batch_size=128
-    learning_rate=2e-3
+    learning_rate=3e-3
     batch_val=2048
     patience=6
     gradient_clipping=10
     nn_filename = 'i3DAE_B3_Complex_New.pth' 
     alp_factor=25
-    aug_flag=True
+    aug_flag=False
     if aug_flag==True:
         batch_size=64
         sigma=0.025
