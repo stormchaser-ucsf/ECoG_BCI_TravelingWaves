8719f1c570fbb53c2c2f432b2573d862d7dd89f2
Nik <nikhilesh.natraj@ucsf.edu>
2026-06-18 22:48:27 +0530

    mods

---
+++ Figure2_Paper.m
@@ -0,0 +1,16 @@
+%% init
+
+clc
+addpath(genpath('/home/user/Documents/Repositories/ECoG_BCI_TravelingWaves/'))
+addpath(genpath('/home/user/Documents/Repositories/ECoG_BCI_HighDim/'))
+cd('/home/user/Documents/Repositories/ECoG_BCI_TravelingWaves')
+
+
+%% LOAD IMAGING DATA FOR THE NEURON DATASET
+
+imaging_EC189
+
+imaging_EC210
+
+imaging_EC176
+
