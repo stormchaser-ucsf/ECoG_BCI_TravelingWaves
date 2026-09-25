e46e224f748d5b59fce6924c131482f80303651b
Nik <nikhilesh.natraj@ucsf.edu>
2026-09-25 22:47:47 +0530

    fixed fig 5 stashed changes

--- Figure5.m
+++ Figure5.m
@@ -898,14 +898,8 @@ cd('C:\Users\nikic\Documents\Ganguly lab\ECoG BCI\BCI_Paper_Waves_Hand\Paper_tex
 
 % svg
 set(gcf,'PaperPositionMode','auto');
-<<<<<<< Updated upstream
-print(gcf,'MuOscillations.svg','-dsvg','-painters','-r300');
-
-=======
 print(gcf,'Colorbar.svg','-dsvg','-painters','-r300');
     
->>>>>>> Stashed changes
-
 % png
 set(gcf,'PaperPositionMode','auto');
 print(gcf,'B3_BrainElec.png','-dpng','-r500');
